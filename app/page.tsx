aad344d714b18afbb263be1a437e1e1287be7add
Ananya-Hegde2001 <ananyavhegde2001@gmail.com>
2025-11-01 15:15:12 +0530

    typo

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="bg-background h-screen w-full text-black flex flex-col justify-between items-center"> 
       <Logo className="text-text-primary opacity-60 mt-1"/> 
       <FlippingCard />
-      <footer className="text-text-primary-one font-mono text-sm mb-1">Design & Developed by Ananya V Hegde 🩵</footer>
+      <footer className="text-text-primary-one font-mono text-sm mb-1">Made with 🩵 by Ananya</footer>
     </div>
   );
 }
